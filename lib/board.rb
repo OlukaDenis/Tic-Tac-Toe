32ffeb52f63eb4c21f36eaa837b381c7e69f59c6
MarioDena <enrique.dn@outlook.com>
2019-11-11 21:07:14 -0600

    Added a mehotd to check winning conditions, finished isvalid? method added a methid to declare a draw, clean up the code on main file

--- lib/board.rb
+++ lib/board.rb
@@ -2,11 +2,37 @@
 
 class Board
   attr_accessor :board
+
   def initialize
-    @board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+    @board = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
   end
 
-  def updated_board(_board)
-    @board = _board
+  def check_for_winner(boardstate)
+    current_b = boardstate[0] + boardstate[1] + boardstate[2]
+
+    if current_b[0] == current_b[1] && current_b[1] == current_b[2]
+      return [true, current_b[0]]
+    elsif current_b[3] == current_b[4] && current_b[4] == current_b[5]
+      return [true, current_b[3]]
+    elsif current_b[6] == current_b[7] && current_b[7] == current_b[8]
+      return [true, current_b[6]]
+
+    elsif current_b[0] == current_b[3] && current_b[3] == current_b[6]
+      return [true, current_b[0]]
+    elsif current_b[1] == current_b[4] && current_b[4] == current_b[7]
+      return [true, current_b[1]]
+    elsif current_b[2] == current_b[5] && current_b[5] == current_b[8]
+      return [true, current_b[2]]
+
+    elsif current_b[0] == current_b[4] && current_b[4] == current_b[8]
+      return [true, current_b[0]]
+    elsif current_b[2] == current_b[4] && current_b[4] == current_b[6]
+      return [true, current_b[2]]
+
+    end
+
+    return[false, nil]
   end
 end
+
+puts Board.new.check_for_winner([%w[X X X], %w[4 5 6], %w[7 8 9]])
--- lib/selection_display.rb
+++ lib/selection_display.rb
@@ -4,10 +4,59 @@ class Selector
   def selection(_player, num, ob)
     ob.each do |n|
       next unless n.include? num
-      
+
       n.map! do |ele|
         ele == num ? _player : ele
       end
     end
+  end
+
+  def is_valid?(num, ob)
+    $selecIndex = convert_to_index(num)
+
+    ob.each_with_index do |n, index|
+      $col = n if index == $selecIndex[0]
+    end
+
+    if $col.include_any?(%w[X O])
+
+      $col.each_with_index do |x, i|
+        if x == 'X' && i == $selecIndex[1]
+          return false
+
+        elsif x == 'O' && i == $selecIndex[1]
+          return false
+        end
+      end
+    end
+    true
+  end
+
+  def convert_to_index(num)
+    if num == '1'
+      [0, 0]
+    elsif num == '2'
+      [0, 1]
+    elsif num == '3'
+      [0, 2]
+    elsif num == '4'
+      [1, 0]
+    elsif num == '5'
+      [1, 1]
+    elsif num == '6'
+      [1, 2]
+    elsif num == '7'
+      [2, 0]
+    elsif num == '8'
+      [2, 1]
+    elsif num == '9'
+      [2, 2]
+    end
+  end
+end
+
+module Enumerable
+  def include_any?(array)
+    array.any? { |i| include? i }
   end
 end
