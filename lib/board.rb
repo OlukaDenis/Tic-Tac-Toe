12a4416e27666ba2e3301ab23a8f0473e8f4a41d
MarioDena <enrique.dn@outlook.com>
2019-11-12 12:30:06 -0600

    Fixed changes for review

--- lib/board.rb
+++ lib/board.rb
@@ -2,9 +2,13 @@
 
 class Board
   attr_accessor :board
+  attr_reader :turn
+  attr_reader :player_one_turn
 
   def initialize
     @board = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
+    @turn = 0
+    @player_one_turn = true
   end
 
   def check_for_winner(boardstate)
@@ -21,5 +25,17 @@ class Board
     return [true, current_b[0]] if current_b[0] == current_b[1] && current_b[1] == current_b[2]
 
     [false, nil]
+  end
+
+  def add_turn
+    @turn += 1
+  end
+
+  def end_game
+    @turn = 10
+  end
+
+  def switch_player
+    @player_one_turn = !@player_one_turn
   end
 end
